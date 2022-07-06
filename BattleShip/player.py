b6cd8a1312b8cb3a4896353d2650b15eaeb0cfd9
aamrlyman <aamrlyman@gmail.com>
2022-07-06 12:29:24 -0700

    add attack function

--- BattleShip/player.py
+++ BattleShip/player.py
@@ -9,3 +9,15 @@ class Player:
         self.destoryer = Ship(2, 'Destoyer')
         self.fleet = [self.carrier, self.battleship, self.cruiser, self.submarine, self.destoryer]
         self.attack_log = []
+    
+    def attack(self):
+        print(self.attack_log)
+        attack_coordinate = input('Type a letter and number in this format (c5) for where you will attack: ')
+        for ship in self.fleet:
+            for coordinate in ship.position:
+                if attack_coordinate == coordinate: 
+                    ship.hitpoints -=1
+                    self.attack_log.append(attack_coordinate)
+                    print(f"You hit your oppoent's {ship.name}. Their {ship.name} has {ship.hitpoints} left.")
+                else:
+                    print('You attack was a miss.')
