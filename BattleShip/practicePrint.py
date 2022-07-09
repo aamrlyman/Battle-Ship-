b30dfd0b2ce9829c02f0f3d7a2348408755df8dc
aamrlyman <aamrlyman@gmail.com>
2022-07-09 14:15:04 -0700

    create gameboard function

---
+++ BattleShip/practicePrint.py
@@ -0,0 +1,21 @@
+Alphabet = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J']
+
+def show_top_of_board():
+        top_of_board = ''
+        for num in range(0, 11):
+            num = str(num)
+            top_of_board += num + "   "
+        print (top_of_board)
+
+def rest_game_board():
+    row = ''
+    for letter in Alphabet:
+        row += letter + '   '
+        for num in range(0,10):
+            num = str(num)
+            row += '__' + '  '
+        print(row)
+        row = ''
+
+show_top_of_board()
+rest_game_board()
